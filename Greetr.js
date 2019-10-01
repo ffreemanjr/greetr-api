a8d55337c0730bda949f7e3331204e93b11392be
Foyah <foyah.genesistechs@gmail.com>
2019-10-01 17:10:42 -0400

    creating structure

--- Greetr.js
+++ Greetr.js
@@ -1,12 +1,23 @@
-(function(global, jQuery) {  //you can replace jQuery with $
+(function(global, jQuery) {  //you can replace the name jQuery with '$'
   
   var Greetr = function(firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   };
 
+  var supportedLangs = ['en', 'es'];
+
+  greetings = {
+
+  };
+
+  formalGreetings = {
+    
+
+  };
+
   Greetr.prototype = {};
 
-  Greetr.init =function(firstName, lastName, language) {
+  Greetr.init = function(firstName, lastName, language) {
 
     //Set some defaults to use if nothing is passed
     var self = this;
@@ -15,5 +26,9 @@
     self.language = language || 'en';
 
   };
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
 
 }(window, $));
